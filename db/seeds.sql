0e288bd969a60755335b5302e7cd181e86249f8e
ink <eric.zmo2@gmail.com>
2020-04-04 12:16:38 -0700

    upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Grill Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('BigMac');
+INSERT INTO burgers (burger_name) VALUES ('Fried Chicken Burger');
+
